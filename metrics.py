4279d2eec52c7b7fc7da91095ecfceaa8cd26854
Rasmus Nylander <s205418@dtu.dk>
2023-06-20 14:27:10 +0200

    metrics: Add function metrics::get_metrics

--- metrics.py
+++ metrics.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision
 from torch import Tensor
+import torchmetrics
 
 @torch.jit.script
 def IoU(rect1: Tensor, rect2: Tensor) -> Tensor:
@@ -12,3 +13,18 @@ def IoU(rect1: Tensor, rect2: Tensor) -> Tensor:
 @torch.jit.script
 def cross_entropy(prediction_logits: Tensor, target: Tensor) -> Tensor:
 	return torch.nn.functional.cross_entropy(prediction_logits, target)
+
+
+def get_metrics() -> list[torchmetrics.Metric]:
+	# precision, recall, f1, accuracy, iou
+	return [
+		torchmetrics.Precision(task="multiclass", num_classes=60, average="macro"),
+		torchmetrics.Recall(task="multiclass", num_classes=60, average="macro"),
+		torchmetrics.F1Score(task="multiclass", num_classes=60, average="macro"),
+		torchmetrics.Accuracy(task="multiclass", num_classes=60, average="macro"),
+		torchmetrics.JaccardIndex(task="multiclass", num_classes=60, average="macro")
+	]
+
+
+if __name__ == "__main__":
+	get_metrics()
