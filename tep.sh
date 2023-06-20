165d170c4c861d1c5e6028365104727f4c3e344c
Rasmus Nylander <s205418@dtu.dk>
2023-06-20 15:11:00 +0200

    Added tep.sh

---
+++ tep.sh
@@ -0,0 +1,27 @@
+MODELS="resnet18 resnet152"
+
+for MODEL in $MODELS
+do
+	
+	n="$MODEL"
+	
+	config="#!/bin/bash
+
+	#BSUB -q gpua40
+	#BSUB -J $n
+	#BSUB -o outs/$n_%J.out
+	#BSUB -n 1
+	#BSUB -R "rusage[mem=20GB]"
+	#BSUB -W 05:00
+	#BSUB -gpu "num=1:mode=exclusive_process"
+
+	module load python3/3.11.3
+	module load cuda/12.1.1
+	source ~/02514/projects/venv/bin/activate
+	"
+	
+
+	command="python train.py --name=$n --model=$MODEL --logging=True --epochs=50"
+	echo "$config$command" | bsub
+	
+done
