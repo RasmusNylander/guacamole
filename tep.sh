0825b1bf0b607872cb693a9233f737af1127612e
Rasmus Nylander <s205418@dtu.dk>
2023-06-20 18:49:54 +0200

    tep: Make batch size 128

--- tep.sh
+++ tep.sh
@@ -7,12 +7,12 @@ do
 	
 	config="#!/bin/bash
 
-	#BSUB -q gpua40
+	#BSUB -q gpua100
 	#BSUB -J $n
 	#BSUB -o outs/$n_%J.out
 	#BSUB -n 1
 	#BSUB -R "rusage[mem=10GB]"
-	#BSUB -W 04:00
+	#BSUB -W 06:00
 	#BSUB -gpu "num=1:mode=exclusive_process"
 
 	module load python3/3.11.3
@@ -21,7 +21,7 @@ do
 	"
 	
 
-	command="python train.py --name=$n --model=$MODEL --logging=True --epochs=30"
+	command="python train.py --name=$n --model=$MODEL --logging=True --epochs=30 --batch_size=128"
 	echo "$config$command" | bsub
 	
 done
