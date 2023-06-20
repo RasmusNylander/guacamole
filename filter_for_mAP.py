09e9e2acfddf741c7b61117ed75ff620eb364dec
HappyCat-9487 <lianglu3366@gmail.com>
2023-06-20 15:23:42 +0200

    update mAP

--- filter_for_mAP.py
+++ filter_for_mAP.py
@@ -12,14 +12,24 @@ def NMS_cal(input_tensor, scores):
     catargmax = torch.argmax(scores, dim=1)
     threshold = 0.5
     
-    combined_tensor = torch.cat((input_tensor, confidscore.unsqueeze(1), catargmax.unsqueeze(1)), dim=1)
-    sorted_indices = torch.argsort(combined_tensor[:, 4:6], dim=1, descending=True)
-    sorted_tensor = torch.gather(combined_tensor, 1, 
-                                 (sorted_indices + 4).unsqueeze(2).expand(-1, -1, combined_tensor.shape[2]))
-    print(sorted_tensor.size())
-    #all_cb = sorted(all_cb,key=lambda x: x[-1])
+    for i in range(confidscore.size(0)):
+        prop = input_tensor[catargmax==1]
+        confid = confidscore
+        
+    #print(confidscore)
+    #print(catargmax)
+    print(input_tensor[catargmax==1])
+    #print(confidscore[catargmax==1])
     
     '''
+    combined_tensor = torch.cat((input_tensor, confidscore.unsqueeze(1), catargmax.unsqueeze(1)), dim=1)
+    sorted_indices = torch.argsort(combined_tensor[:, 4:6], dim=1, descending=True)
+    sorted_tensor = torch.cat((combined_tensor[:, :4], combined_tensor[:, 4:6].gather(1, sorted_indices)), dim=1)
+    print(combined_tensor[0])
+    print(sorted_tensor[0])
+    #all_cb = sorted(all_cb,key=lambda x: x[-1])
+    
+    
     
     i = 0
     for c in (catargmax.size(0)):
--- mAP.py
+++ mAP.py
@@ -10,7 +10,7 @@ def precision_recall_curve(y_true, pred_scores, thresholds):
     recalls = []
     
     for threshold in thresholds:
-        y_pred = ["positive" if score >= threshold else "negative" for score in pred_scores]
+        y_pred = [1 if score >= threshold else 0 for score in pred_scores]
         
         precision = sklearn.metrics.precision_score(y_true=y_true, y_pred=y_pred, pos_label="positive")
         recall = sklearn.metrics.recall_score(y_true=y_true, y_pred=y_pred, pos_label="positive")
@@ -51,39 +51,9 @@ def intersection_over_union(gt_box, pred_box):
 def mAP_cal(ap_values):
     mAP = sum(ap_values)/len(ap_values)
     return mAP
-    
 
 
-if __name__ == '__main__':
-    path = './precision-recall_curve'
-    num = 1 #TODO: Input the number for the precision-recall number if you want to generate the graphs
-    
-    #For testing 
-    pred_scores = [0.7, 0.3, 0.5, 0.6, 0.55, 0.9, 0.4, 0.2, 0.4, 0.3]  #TODO: The prediction input
-    y_true = ["positive", "negative", "negative", "positive", "positive", 
-            "positive", "negative", "positive", "negative", "positive"] #TODO: Input the true 
-    
-    threshold = 0.5
-    y_pred = ["positive" if score >= threshold else "negative" for score in pred_scores]
-    #print(y_pred)
-
-    r = np.flip(sklearn.metrics.confusion_matrix(y_true, y_pred))
-    
-    
-    precision = sklearn.metrics.precision_score(y_true=y_true, y_pred=y_pred, pos_label="positive")
-    recall = sklearn.metrics.recall_score(y_true=y_true, y_pred=y_pred, pos_label="positive")
-    thresholds = np.arange(start=0.2, stop=0.7, step=0.05)
-
-    precisions, recalls = precision_recall_curve(y_true=y_true, pred_scores=pred_scores,
-                                                thresholds=thresholds)
-
-    f1 = f1_score(precisions, recalls)  #Calclate the buest tradeoff point from the pairs of precisions and recalls.
-    #One category situation
-    AP = AP_cal(precisions, recalls)
-    print(AP)
-    
-    
-    #Store the image of precision-recall curve
+def Precision_recall_curve_plot(precisions,recalls, AP, path, num):
     plt.plot(recalls, precisions, linewidth=4, color="red", zorder=0)
     plt.scatter(recalls[the_maximum_index(f1)], 
                 precisions[the_maximum_index(f1)], 
@@ -95,23 +65,43 @@ if __name__ == '__main__':
     plt.title("Precision-Recall Curve", fontsize=15, fontweight="bold")
     os.makedirs(path, exist_ok=True)
     plt.savefig(f"{path}/test_{num}.png")
-
     
+
+
+if __name__ == '__main__':
+    
+    path = './precision-recall_curve'
+    APs = []
+    
+    for i in range(59):
+        num = 1 + i  #TODO: Input the number for the precision-recall number if you want to generate the graphs
+        
+        pred_scores = [0.7, 0.3, 0.5, 0.6, 0.55, 0.9, 0.4, 0.2, 0.4, 0.3]  #TODO: The prediction input
+        y_true = [1, 0, 0, 1, 1, 1, 0, 1, 0, 1] #TODO: Input the true
+
+        
+        thresholds = np.arange(start=0.2, stop=1.0, step=0.05)
+
+        precisions, recalls = precision_recall_curve(y_true=y_true, pred_scores=pred_scores,
+                                                    thresholds=thresholds)
+
+        f1 = f1_score(precisions, recalls)  #Calclate the buest tradeoff point from the pairs of precisions and recalls.
+        
+        #One category situation
+        AP = AP_cal(precisions, recalls)
+        APs.append(AP)  # The list will contain various APs
+        
+        #Store the image of precision-recall curve
+        Precision_recall_curve_plot(precisions,recalls, AP, path, num)
+         
     '''
     #IoU related testing
     gt_box = [320, 220, 680, 900] #Pretend the position of ground box will be performed a list
     pred_box = [500, 320, 550, 700]  #Pretend the position of prediction box will be performed a list
     iou, intersect, union = intersection_over_union(gt_box, pred_box)
     print('IoU: {iou} \nIntersect: {intersect} \nUnion: {union}')
-    
-    # mAP: Mean Average Precision (for multiple categories)
-    l_APs = [] # The list of various APs
-    mAP = mAP_cal(l_APs)
-    print('Mean Average Precision: ' + str(mAP))
     '''
+    # mAP: Mean Average Precision (for multiple categories)
+    mAP = mAP_cal(APs)
+    print('Mean Average Precision: ' + str(mAP))
     
-    
-    
-    
-
-
